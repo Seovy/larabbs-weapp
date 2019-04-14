806713218ce283ae04880aacf979052d2fc56cab
davie <807364811@qq.com>
2019-04-14 10:17:38 +0800

    add moment.js

---
+++ src/utils/util.js
@@ -0,0 +1,12 @@
+import moment from 'moment'
+
+import 'moment/locale/zh-cn'
+
+const diffForHumans = (date, format="YYYYMMDD H:mm:ss") => {
+    moment.locale('zh-cn')
+    return moment(date,format).fromNow()
+}
+
+export default {
+    diffForHumans
+}
